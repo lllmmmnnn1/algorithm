28e242e352f8b30673859a932b10c51d25741285
SC-201806040710\Administrator <17717224650@163.com>
2021-01-07 22:50:00 +0800

    有序二分

---
+++ base/src/com/algo/base/BSExist.java
@@ -0,0 +1,28 @@
+package com.algo.base;
+
+/**
+ * 二分 有序
+ */
+public class BSExist {
+    public static boolean exist(int[] sortedArr, int num) {
+
+        if (sortedArr == null || sortedArr.length == 0) {
+            return false;
+        }
+        int L = 0;
+        int R = sortedArr.length - 1;
+        int mid = 0;
+        while (L < R) {
+            mid = L + ((R - L) >> 1); //mid = (L+R)/2
+            if (sortedArr[mid] == num) {
+                return true;
+            } else if (sortedArr[mid] > num) {
+                R = mid - 1;
+            } else {
+                L = mid + 1;
+            }
+
+        }
+        return sortedArr[L] == num;
+    }
+}
